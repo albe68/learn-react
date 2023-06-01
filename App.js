da7f1ac01ceaa708c6f90796ebcfb994382aa8ad
Albert Remalin Raja <albertpaulus836@gmail.com>
2023-06-01 16:35:33 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,7 @@
+import React  from "react";
+import ReactDOM from "react-dom";
+
+console.log('file haha')
+const heading=React.createElement("h1",{id:"title"},"heading 1")
+
+ReactDOM.render(heading, document.getElementById("root"));
